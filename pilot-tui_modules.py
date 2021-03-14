f772fb8fdb497ec6de7a2f2dd22fa32db47589ea
bileyg <bileyg@yandex.ru>
2021-03-14 21:53:24 +0300

    modules from folder

--- pilot-tui_modules.py
+++ pilot-tui_modules.py
@@ -23,6 +23,9 @@
 #  
 
 import sys,os
+
+sys.path.insert(1, './modules')
+
 import curses
 import subprocess
 import resource
@@ -37,31 +40,32 @@ import setUp
 import testServer
 import buildComplex
 import connectDemoBases
+import renderImage
 
 def draw_menu(stdscr, tilist, conflist, Fconf, F_Done):
     
-    def renderImage():
-        # Rendering logo
-        # from the terminal image strings
-        PixelMatrix = []
-        PixelString = []
-        PixelColors = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-        if height >= 23 and width >= 80:
-            for j in range(3,22):
-                PixelString = tilist[j]
-                for i in range(3,82):
-                    Pixel = tilist[j][i]
-                    PColor = PixelColors.index(Pixel)
-                    stdscr.attron(curses.color_pair(20+PColor))
-                    if PColor > 7:
-                        stdscr.attron(curses.A_BOLD)
-                    XX = (center_x - (len(PixelString)-4)//2)
-                    YY = (center_y - 11)
-                    stdscr.addstr(j-1+YY, i-3+XX, B)
-                    #stdscr.addstr(start_y+j-11, start_x_logo+i, 'B')
-                    if PColor > 7:
-                        stdscr.attroff(curses.A_BOLD)
-                    stdscr.attroff(curses.color_pair(20+PColor))
+    #def renderImage():
+        ## Rendering logo
+        ## from the terminal image strings
+        #PixelMatrix = []
+        #PixelString = []
+        #PixelColors = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+        #if height >= 23 and width >= 80:
+            #for j in range(3,22):
+                #PixelString = tilist[j]
+                #for i in range(3,82):
+                    #Pixel = tilist[j][i]
+                    #PColor = PixelColors.index(Pixel)
+                    #stdscr.attron(curses.color_pair(20+PColor))
+                    #if PColor > 7:
+                        #stdscr.attron(curses.A_BOLD)
+                    #XX = (center_x - (len(PixelString)-4)//2)
+                    #YY = (center_y - 11)
+                    #stdscr.addstr(j-1+YY, i-3+XX, B)
+                    ##stdscr.addstr(start_y+j-11, start_x_logo+i, 'B')
+                    #if PColor > 7:
+                        #stdscr.attroff(curses.A_BOLD)
+                    #stdscr.attroff(curses.color_pair(20+PColor))
     
     def renderSubtitle():
         # Rendering subtitle
